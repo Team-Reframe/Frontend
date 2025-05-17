93c1abff291db17cfb19d5eb610160d5ee4935b3
UJIN\dnwls <dnwls0254@naver.com>
2025-05-17 17:03:49 +0900

    뷰바인딩 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.reframe"
     compileSdk = 35
 
+    viewBinding{
+        enable = true
+    }
+
     defaultConfig {
         applicationId = "com.example.reframe"
         minSdk = 34
